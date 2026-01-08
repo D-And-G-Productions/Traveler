1cdd3a235d516b4eadc2710d25a86be79f7a6598
IIDCII <darioclinemp3@gmail.com>
2026-01-08 23:50:28 +0000

    got rid of dotnet

--- traveler_web/FirebaseConfig.ts
+++ traveler_web/FirebaseConfig.ts
@@ -1,23 +1,21 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import {
-    FIREBASE_API_KEY,
-    FIREBASE_AUTH_DOMAIN,
-    FIREBASE_PROJECT_ID,
-    FIREBASE_STORAGE_BUCKET,
-    FIREBASE_MESSAGING_SENDER_ID,
-    FIREBASE_APP_ID
-    // @ts-ignore: the env import works
-} from '@env';
-
+// const firebaseConfig = {
+//     apiKey: "AIzaSyDHK29uOINgapSmoHsg0b_6DxwQX6VyvMA",
+//     authDomain: "traveler-a3efb.firebaseapp.com",
+//     projectId: "traveler-a3efb",
+//     storageBucket: "traveler-a3efb.firebasestorage.app",
+//     messagingSenderId: "608904282716",
+//     appId: "1:608904282716:web:09203ef65f1a1d8adf9c99"
+// };
 const firebaseConfig = {
-    apiKey: FIREBASE_API_KEY,
-    authDomain: FIREBASE_AUTH_DOMAIN,
-    projectId: FIREBASE_PROJECT_ID,
-    storageBucket: FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
-    appId: FIREBASE_APP_ID
+    apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
--- traveler_web/babel.config.js
+++ traveler_web/babel.config.js
@@ -2,14 +2,5 @@ module.exports = function (api) {
     api.cache(true);
     return {
         presets: ['babel-preset-expo'], // CHANGE THIS LINE
-        plugins: [
-            [
-                'module:react-native-dotenv',
-                {
-                    moduleName: '@env',
-                    path: '.env',
-                },
-            ],
-        ],
     };
 };
