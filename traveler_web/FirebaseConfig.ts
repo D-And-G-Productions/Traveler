161c44a7c0b8d6a38b944c125228aaf95e3f47af
IIDCII <darioclinemp3@gmail.com>
2026-01-09 00:04:01 +0000

    altered the firebaseconfig file

--- traveler_web/FirebaseConfig.ts
+++ traveler_web/FirebaseConfig.ts
@@ -1,14 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-// const firebaseConfig = {
-//     apiKey: "AIzaSyDHK29uOINgapSmoHsg0b_6DxwQX6VyvMA",
-//     authDomain: "traveler-a3efb.firebaseapp.com",
-//     projectId: "traveler-a3efb",
-//     storageBucket: "traveler-a3efb.firebasestorage.app",
-//     messagingSenderId: "608904282716",
-//     appId: "1:608904282716:web:09203ef65f1a1d8adf9c99"
-// };
+
 const firebaseConfig = {
     apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
     authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
