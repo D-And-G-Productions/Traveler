a6047a416b1928ff246f4a3447fb5ffd36b3ecb5
G-Baudelaire <germainjones1@gmail.com>
2026-01-16 13:54:25 +0000

    Add migration guide in setup

--- migrations/scripts/migrate_down.sh
+++ migrations/scripts/migrate_down.sh
@@ -6,4 +6,4 @@ SCRIPT_DIRECTORY="$(dirname "$0")"
 PARENT_DIRECTORY="$SCRIPT_DIRECTORY/.."
 cd "$PARENT_DIRECTORY"
 
-migrate -database "$DATABASE_URL?sslmode=disable" -path . down
+migrate -database "$DATABASE_URL" -path . down
--- migrations/scripts/migrate_up.sh
+++ migrations/scripts/migrate_up.sh
@@ -6,4 +6,4 @@ SCRIPT_DIRECTORY="$(dirname "$0")"
 PARENT_DIRECTORY="$SCRIPT_DIRECTORY/.."
 cd "$PARENT_DIRECTORY"
 
-migrate -database "$DATABASE_URL?sslmode=disable" -path . up
+migrate -database "$DATABASE_URL" -path . up
